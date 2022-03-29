079eec1a7bb3081817a90b908ee95195e9c0d8f4
Zork <yana_zorkaya2@mail.ru>
2022-03-29 11:36:49 +0300

    clear

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ import Box from '@mui/material/Box';
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
-  textColor: white;
+  text: white;
 `;
 
 export const AppFooter = () => {
@@ -12,7 +12,9 @@ export const AppFooter = () => {
         <footer>
             <Box sx={{ backgroundColor: '#343a40', padding: 1}}>
                 {/* Возвращает на Add */}
+                <Wrapper>
                 © { new Date().getFullYear() } <a href="index.html">CarLogo</a>. Все права на опубликованные изображения принадлежат их авторам или законным владельцам.
+                </Wrapper>
             </Box>
         </footer>
     )
